2a1d7de8d90086754514c2a6da59f5fe99a2f247
Kichul Kim <hi.jigoo@gmail.com>
2026-03-06 22:46:23 +0900

    chore: Java 스텝별 build.gradle.kts 테스트 의존성 추가

--- java/step-07-advanced/complete/build.gradle.kts
+++ java/step-07-advanced/complete/build.gradle.kts
@@ -22,6 +22,8 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     runtimeOnly("com.h2database:h2")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
 tasks.withType<Test> {
