20dd84db0fa05e95818d756b9e9814b19f203b36
Kichul Kim <hi.jigoo@gmail.com>
2026-03-15 01:58:39 +0900

    step-08: starter build.gradle.kts, application.properties 설정 보강 및 테스트 파일 추가

---
+++ java/step-08-advanced/starter/src/test/java/com/example/todo/TodoApplicationTests.java
@@ -0,0 +1,15 @@
+package com.example.todo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * 애플리케이션 컨텍스트가 정상적으로 로드되는지 확인하는 테스트.
+ */
+@SpringBootTest
+class TodoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
